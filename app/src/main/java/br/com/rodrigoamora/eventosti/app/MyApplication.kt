91580d5ed5920d12a0e5d60c8e83bdb3f649f778
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-11-03 01:46:14 -0300

    Criando a classe MyApplication

---
+++ app/src/main/java/br/com/rodrigoamora/eventosti/app/MyApplication.kt
@@ -0,0 +1,11 @@
+package br.com.rodrigoamora.eventosti.app
+
+import android.app.Application
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
