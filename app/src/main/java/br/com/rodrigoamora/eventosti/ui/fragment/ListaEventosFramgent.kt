9edcc8a22014082b8a952243712a6003784884d9
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-05-04 04:15:00 -0300

    Removendo imports não usados

--- app/src/main/java/br/com/rodrigoamora/eventosti/ui/fragment/ListaEventosFramgent.kt
+++ app/src/main/java/br/com/rodrigoamora/eventosti/ui/fragment/ListaEventosFramgent.kt
@@ -5,9 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.navigation.Navigation
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
