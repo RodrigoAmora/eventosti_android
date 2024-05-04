0e6d1f36ee3c8eb167dd7bf7ce7569ba9302fcb6
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-05-03 22:52:58 -0300

    Adicionando os atributos

---
+++ app/src/main/java/br/com/rodrigoamora/eventosti/model/Evento.kt
@@ -0,0 +1,27 @@
+package br.com.rodrigoamora.eventosti.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+@Entity
+data class Evento (
+
+    @PrimaryKey
+    @SerializedName("id")
+    var id: Long,
+
+    @SerializedName("nome")
+    var nome: String,
+
+    @SerializedName("site")
+    var site: String,
+
+    @SerializedName("dataInicio")
+    var dataInicio: String,
+
+    @SerializedName("dataFim")
+    var dataFim: String
+
+): Serializable
