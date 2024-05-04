1d1e80637f7127be05b4f1153207744dcac3383d
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-05-04 04:04:35 -0300

    Injetando dependências

--- app/src/main/java/br/com/rodrigoamora/eventosti/app/MyApplication.kt
+++ app/src/main/java/br/com/rodrigoamora/eventosti/app/MyApplication.kt
@@ -1,11 +1,39 @@
 package br.com.rodrigoamora.eventosti.app
 
 import android.app.Application
+import br.com.rodrigoamora.eventosti.di.injectFeature
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 
 class MyApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        this.instantiateKoin()
+        this.instantiateApplicationScope()
     }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        this.terminateKoin()
+    }
+
+    private fun instantiateKoin() {
+        startKoin{
+            androidLogger()
+            androidContext(this@MyApplication)
+            injectFeature()
+        }
+    }
+
+    private fun terminateKoin() {
+        stopKoin()
+    }
+
+    private fun instantiateApplicationScope(): CoroutineScope = CoroutineScope(SupervisorJob())
 
 }
---
+++ app/src/main/java/br/com/rodrigoamora/eventosti/di/KoinModules.kt
@@ -0,0 +1,62 @@
+package br.com.rodrigoamora.eventosti.di
+
+import androidx.room.Room
+import br.com.rodrigoamora.eventosti.BuildConfig
+import br.com.rodrigoamora.eventosti.database.AppRoomDatabase
+import br.com.rodrigoamora.eventosti.network.retorift.AppRetrofit
+import br.com.rodrigoamora.eventosti.network.retorift.webclient.EventoWebClient
+import br.com.rodrigoamora.eventosti.repository.EventoRepository
+import br.com.rodrigoamora.eventosti.repository.impl.EventoRepositoryImpl
+import br.com.rodrigoamora.eventosti.ui.viewmodel.EventoViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.GlobalContext.loadKoinModules
+import org.koin.dsl.module
+
+fun injectFeature() = loadFeature
+
+private val loadFeature by lazy {
+    loadKoinModules(
+        listOf(
+            databaseModule,
+            repositoryModule,
+            retrofitModule,
+            servicesModule,
+            viewModelModule,
+            webClientModule
+        )
+    )
+}
+
+val databaseModule = module {
+    single {
+        Room.databaseBuilder(
+            get(),
+            AppRoomDatabase::class.java,
+            BuildConfig.DATABASE_NAME)
+            .allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    single { get<AppRoomDatabase>().eventoDao() }
+}
+
+val repositoryModule = module {
+    single<EventoRepository> { EventoRepositoryImpl(get(), get()) }
+}
+
+val retrofitModule = module {
+    single { AppRetrofit(BuildConfig.BASE_URL_API).instantiateRetrofit() }
+}
+
+val servicesModule = module {
+    single { AppRetrofit(BuildConfig.BASE_URL_API).eventoService() }
+}
+
+val viewModelModule = module {
+    viewModel { EventoViewModel(get()) }
+}
+
+val webClientModule = module {
+    single { EventoWebClient(get()) }
+}
