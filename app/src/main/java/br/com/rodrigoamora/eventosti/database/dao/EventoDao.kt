e24e17194d404c2e7e8df8683e8478603e1a3334
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-05-03 22:52:25 -0300

    Criando a classe AppRoomDatabase e a interface EventoDao

---
+++ app/src/main/java/br/com/rodrigoamora/eventosti/database/dao/EventoDao.kt
@@ -0,0 +1,26 @@
+package br.com.rodrigoamora.eventosti.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.Companion.REPLACE
+import androidx.room.Query
+import br.com.rodrigoamora.eventosti.model.Evento
+
+@Dao
+interface EventoDao {
+
+    @Query("SELECT * FROM Evento")
+    fun listarTodos(): LiveData<List<Evento>>
+
+    @Insert(onConflict = REPLACE)
+    fun save(evento: Evento)
+
+    @Insert(onConflict = REPLACE)
+    fun save(eventos: List<Evento>)
+
+    @Delete
+    fun remove(evento: Evento)
+
+}
