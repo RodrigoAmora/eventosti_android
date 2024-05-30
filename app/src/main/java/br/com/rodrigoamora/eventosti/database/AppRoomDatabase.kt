d7eae7c94bf059578785e83febad0b8a20d93ecc
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-05-30 00:18:49 -0300

    Removendo imports não usados

--- app/src/main/java/br/com/rodrigoamora/eventosti/database/AppRoomDatabase.kt
+++ app/src/main/java/br/com/rodrigoamora/eventosti/database/AppRoomDatabase.kt
@@ -2,12 +2,10 @@ package br.com.rodrigoamora.eventosti.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import br.com.rodrigoamora.eventosti.database.converter.Converters
 import br.com.rodrigoamora.eventosti.database.dao.EventoDao
 import br.com.rodrigoamora.eventosti.model.Evento
 
-//@TypeConverters(Converters::class)
+
 @Database(entities = [Evento::class], version = 1, exportSchema = false)
 abstract class AppRoomDatabase: RoomDatabase() {
     abstract fun eventoDao(): EventoDao
