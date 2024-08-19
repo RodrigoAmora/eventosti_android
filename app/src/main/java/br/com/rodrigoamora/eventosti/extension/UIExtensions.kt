f9b605dfbd70c18d25ab5d63125e81d14ccaa360
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-19 01:05:20 -0300

    Criando extensions para a classe SearchView

--- app/src/main/java/br/com/rodrigoamora/eventosti/extension/UIExtensions.kt
+++ app/src/main/java/br/com/rodrigoamora/eventosti/extension/UIExtensions.kt
@@ -2,6 +2,7 @@ package br.com.rodrigoamora.eventosti.extension
 
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.SearchView
 
 //ProgressBar
 fun ProgressBar.hide() {
@@ -9,5 +10,15 @@ fun ProgressBar.hide() {
 }
 
 fun ProgressBar.show() {
+    this.visibility = View.VISIBLE
+}
+
+
+//SearchView
+fun SearchView.hide() {
+    this.visibility = View.GONE
+}
+
+fun SearchView.show() {
     this.visibility = View.VISIBLE
 }
