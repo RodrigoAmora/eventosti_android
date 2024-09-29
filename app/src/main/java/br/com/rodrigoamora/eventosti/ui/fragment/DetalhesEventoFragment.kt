5ef1d36e767a7e6822ab0f10cd1238970c435ea0
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-09-29 01:45:18 -0300

    Removendo o ImageView

--- app/src/main/java/br/com/rodrigoamora/eventosti/ui/fragment/DetalhesEventoFragment.kt
+++ app/src/main/java/br/com/rodrigoamora/eventosti/ui/fragment/DetalhesEventoFragment.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.ProgressBar
 import android.widget.TextView
-import br.com.rodrigoamora.eventosti.BuildConfig
 import br.com.rodrigoamora.eventosti.R
 import br.com.rodrigoamora.eventosti.databinding.FragmentDetalhesEventoBinding
 import br.com.rodrigoamora.eventosti.model.Evento
 import br.com.rodrigoamora.eventosti.ui.activiy.MainActivity
-import br.com.rodrigoamora.eventosti.util.ShareUtil
 
 class DetalhesEventoFragment: BaseFragment() {
 
@@ -24,7 +20,6 @@ class DetalhesEventoFragment: BaseFragment() {
     private lateinit var tvDescricaoEvento: TextView
     private lateinit var tvSiteEvento: TextView
     private lateinit var tvTipoEvento: TextView
-    private lateinit var ivShare: ImageView
 
     private val mainActivity: MainActivity by lazy {
         activity as MainActivity
@@ -66,11 +61,5 @@ class DetalhesEventoFragment: BaseFragment() {
 
         this.tvTipoEvento = this.binding.tvTipoEvento
         this.tvTipoEvento.text = getString(R.string.tv_tipo_evento, evento.tipoEvento)
-
-        this.ivShare = this.binding.ivShare
-        this.ivShare.setOnClickListener {
-            val texto = BuildConfig.BASE_URL_API+"/verEvento?id="+evento.id
-            ShareUtil.directShare(this.mainActivity, evento.nome, texto)
-        }
     }
 }
